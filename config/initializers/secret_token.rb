0177a6b786200b272a9ffedd0214479cf38f4e4a
Tyler Tringas <tyler@tylertringas.com>
2013-05-18 13:10:06 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SolarCanvasser::Application.config.secret_token = '92fb6475c2eff1007b82e0fced3410d21a188f2e942f097732a25daffa18893c6b63abd36f127191cff5a60ff4c77d9ff9349480c908f496c28bbc57507521bb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SolarCanvasser::Application.config.session_store :cookie_store, key: '_solar_canvasser_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SolarCanvasser::Application.config.session_store :active_record_store
